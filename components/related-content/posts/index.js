3617a3b568487cb21fa1449e73bbee625b59bd6d
Ed Brett <e.j.a.brett@gmail.com>
2020-09-11 22:54:57 +0100

    add correct links to blogs

--- components/related-content/posts/index.js
+++ components/related-content/posts/index.js
@@ -10,23 +10,49 @@ import Card from 'components/card';
 const Posts = ({ posts: articles, maxCols }) => {
   return (
     <Row nested>
-      {articles?.map(({ id, excerpt, featured_media: media, ...rest }) => (
-        <Column
-          key={id}
-          width={[1, 1 / 2, 1 / (maxCols || 2)]}
-          css={css`
-            margin-bottom: 30px;
-          `}
-        >
-          <Card
-            {...rest}
-            excerpt={ReactHtmlParser(excerpt.rendered)}
-            {...(media && {
-              media,
-            })}
-          />
-        </Column>
-      ))}
+      {articles?.map(
+        ({ id, excerpt, featured_media: media, link, categories, ...rest }) => (
+          <Column
+            key={id}
+            width={[1, 1 / 2, 1 / (maxCols || 2)]}
+            css={css`
+              margin-bottom: 30px;
+            `}
+          >
+            <div
+              css={css`
+                position: relative;
+              `}
+            >
+              <a
+                href={`https://blog.globalforestwatch.org${link}`}
+                alt={rest?.title}
+                css={css`
+                  z-index: 1;
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                `}
+              >
+                &nbsp;
+              </a>
+              <Card
+                {...rest}
+                categories={categories?.map((cat) => ({
+                  ...cat,
+                  link: `https://blog.globalforestwatch.org${cat.link}`,
+                }))}
+                excerpt={ReactHtmlParser(excerpt.rendered)}
+                {...(media && {
+                  media,
+                })}
+              />
+            </div>
+          </Column>
+        )
+      )}
     </Row>
   );
 };
