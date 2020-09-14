04036482d0a525186fcb266c2ea6334573d6f0fb
Ed Brett <e.j.a.brett@gmail.com>
2020-09-14 13:16:02 +0100

    add preview banner component

---
+++ components/preview-banner/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Row, Column } from 'gfw-components';
+
+import { Wrapper, Container, CloseIcon } from './styles';
+
+const PreviewBanner = () => (
+  <Wrapper>
+    <Row>
+      <Column>
+        <Container>
+          <a href="/help/api/exit-preview">
+            PREVIEW MODE
+            <CloseIcon />
+          </a>
+        </Container>
+      </Column>
+    </Row>
+  </Wrapper>
+);
+
+export default PreviewBanner;
---
+++ components/preview-banner/styles.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+import { theme } from 'gfw-components';
+
+import CloseIconSrc from 'assets/icons/close.svg';
+
+export const Wrapper = styled.div`
+  background-color: ${theme.colors.darkGrey};
+`;
+
+export const Container = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  color: ${theme.colors.white};
+  font-size: 14px;
+
+  a {
+    color: inherit;
+  }
+`;
+
+export const CloseIcon = styled(CloseIconSrc)`
+  width: 10px;
+  height: 10px;
+  fill: ${theme.colors.white};
+  margin-left: 10px;
+`;
