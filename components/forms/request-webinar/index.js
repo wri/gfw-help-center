b8f7d1ef2d21c0df7efa1e2e7a9f9d9180eeae33
Ed Brett <e.j.a.brett@gmail.com>
2020-09-15 07:23:36 +0100

    remove request a webinar text

--- components/forms/request-webinar/index.js
+++ components/forms/request-webinar/index.js
@@ -1,51 +1,66 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { Form, Submit, Checkbox, Input, validations, Loader } from 'gfw-components';
+import {
+  Form,
+  Submit,
+  Checkbox,
+  Input,
+  validations,
+  Loader,
+} from 'gfw-components';
 
 import Message from '../../message';
 
 import postRequest from './actions';
-import { AgreeBoxWrapper, Title, Description } from './styles';
+import { AgreeBoxWrapper, Title } from './styles';
 
 const RequestForm = () => {
   const { email } = validations;
 
   return (
     <Form onSubmit={postRequest} initialValues={{ agree: ['agree'] }}>
-      {({ handleSubmit, submitting, submitSucceeded }) => submitSucceeded ? (
-        <Message small title="Request sent!" description="We will get back to you as soon as we can." />
-      ) : (
-        <>
-          {submitting && <Loader />}
-          <Title>Request a webinar topic</Title>
-          <Description>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</Description>
-          <form onSubmit={handleSubmit}>
-            <Input type="textarea" name="request" label="request" />
-            <Input name="name" label="name" required />
-            <Input type="email" name="email" label="email" validate={[email]} placeholder="example@globalforestwatch.org" required />
-            <AgreeBoxWrapper>
-              <Checkbox
-                name="agree"
-                options={[
-                  {
-                    label: "I have read and agree to GFW's privacy policy",
-                    value: 'agree',
-                  },
-                ]}
+      {({ handleSubmit, submitting, submitSucceeded }) =>
+        submitSucceeded ? (
+          <Message
+            small
+            title="Request sent!"
+            description="We will get back to you as soon as we can."
+          />
+        ) : (
+          <>
+            {submitting && <Loader />}
+            <Title>Request a webinar topic</Title>
+            <form onSubmit={handleSubmit}>
+              <Input type="textarea" name="request" label="request" />
+              <Input name="name" label="name" required />
+              <Input
+                type="email"
+                name="email"
+                label="email"
+                validate={[email]}
+                placeholder="example@globalforestwatch.org"
                 required
               />
-            </AgreeBoxWrapper>
-            <Submit submitting={submitting}>
-              Send request
-            </Submit>
-          </form>
-        </>
-      )}
+              <AgreeBoxWrapper>
+                <Checkbox
+                  name="agree"
+                  options={[
+                    {
+                      label: "I have read and agree to GFW's privacy policy",
+                      value: 'agree',
+                    },
+                  ]}
+                  required
+                />
+              </AgreeBoxWrapper>
+              <Submit submitting={submitting}>Send request</Submit>
+            </form>
+          </>
+        )}
     </Form>
   );
 };
 
-RequestForm.propTypes = {
-};
+RequestForm.propTypes = {};
 
 export default RequestForm;
--- components/forms/request-webinar/styles.js
+++ components/forms/request-webinar/styles.js
@@ -1,15 +1,9 @@
-import styled from '@emotion/styled'
-import { H4, P } from 'gfw-components';
+import styled from '@emotion/styled';
+import { H4 } from 'gfw-components';
 
 export const Title = styled(H4)`
-  margin-bottom: 20px;
-`
-
-export const Description = styled(P)`
-  font-size: 20px;
-  line-height: 36px;
-  margin-bottom: 50px;
-`
+  margin-bottom: 40px;
+`;
 
 export const AgreeBoxWrapper = styled.div`
   margin-top: -10px;
