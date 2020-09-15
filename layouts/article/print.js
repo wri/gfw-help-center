3638c74307e253886d456c28a969282e35b7be60
Ed Brett <e.j.a.brett@gmail.com>
2020-09-15 15:46:37 +0100

    add print margins for pages

--- layouts/article/print.js
+++ layouts/article/print.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
+import { Global, css } from '@emotion/core';
 import ReactHtmlParser from 'react-html-parser';
 
 import PostContent from 'components/content';
@@ -14,30 +14,47 @@ class ComponentToPrint extends Component {
     const { title, content } = article || {};
 
     return (
-      <div
-        css={css`
-          padding: 60px 100px;
-        `}
-      >
-        <p
+      <>
+        <Global
           css={css`
-            margin-bottom: 5px;
+            @media print {
+              html,
+              body {
+                height: initial !important;
+                overflow: initial !important;
+                -webkit-print-color-adjust: exact;
+              }
+            }
+
+            @page {
+              size: auto;
+              margin: 25mm 25mm 30mm;
+            }
           `}
-        >
-          Global Forest Watch Help Center
-        </p>
-        <p
-          css={css`
-            margin-bottom: 50px;
-          `}
-        >
-          https://www.globalforestwatch.org/help/
-        </p>
-        <PostTitle className="notranslate">{ReactHtmlParser(title)}</PostTitle>
-        <PostContent align="left">
-          {ReactHtmlParser(content.rendered)}
-        </PostContent>
-      </div>
+        />
+        <div>
+          <p
+            css={css`
+              margin-bottom: 5px;
+            `}
+          >
+            Global Forest Watch Help Center
+          </p>
+          <p
+            css={css`
+              margin-bottom: 50px;
+            `}
+          >
+            https://www.globalforestwatch.org/help
+          </p>
+          <PostTitle className="notranslate">
+            {ReactHtmlParser(title)}
+          </PostTitle>
+          <PostContent align="left">
+            {ReactHtmlParser(content.rendered)}
+          </PostContent>
+        </div>
+      </>
     );
   }
 }
