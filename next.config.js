cb6f0eb5e8348291a3178737b203c80e8230b23e
Ed Brett <e.j.a.brett@gmail.com>
2020-10-12 23:12:14 +0100

    add redirects in node build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const fetch = require('node-fetch');
+const btoa = require('btoa');
 
 module.exports = withPlugins(
   [[optimizedImages, { handleImages: ['jpeg', 'png', 'webp', 'gif'] }]],
@@ -20,5 +22,27 @@ module.exports = withPlugins(
       return config;
     },
     trailingSlash: true,
+    async redirects() {
+      const userPassword = btoa(
+        `${process.env.NEXT_PUBLIC_AUTH_USER}:${process.env.NEXT_PUBLIC_AUTH_TOKEN}`
+      );
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/redirection/v1/redirect?per_page=200&filterBy[status]=enabled&filterBy[url]=/help/`,
+        {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${userPassword}`,
+          },
+        }
+      );
+      const json = await response.json();
+
+      return json.items.map((r) => ({
+        source: `${r.url}/`,
+        destination: `${r.action_data.url}/`,
+        permanent: true,
+      }));
+    },
   }
 );
