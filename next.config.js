0a78dd15993f72540c579e4f507d680da98ffe53
Ed Brett <e.j.a.brett@gmail.com>
2020-10-13 09:30:50 +0100

    fetch all redirects at build by page

--- next.config.js
+++ next.config.js
@@ -26,19 +26,40 @@ module.exports = withPlugins(
       const userPassword = btoa(
         `${process.env.NEXT_PUBLIC_AUTH_USER}:${process.env.NEXT_PUBLIC_AUTH_TOKEN}`
       );
+      const fetchConfig = {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${userPassword}`,
+        },
+      };
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/redirection/v1/redirect?per_page=200&filterBy[status]=enabled&filterBy[url]=/help/`,
-        {
-          method: 'get',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ${userPassword}`,
-          },
-        }
+        `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/redirection/v1/redirect?per_page=5&filterBy[status]=enabled&filterBy[url]=/help/`,
+        fetchConfig
       );
       const json = await response.json();
+      const totalPages = Math.ceil(parseInt(json.total, 10) / 50);
+      const redirectPages = await Promise.all(
+        Array.from(Array(totalPages).keys()).map((page) =>
+          fetch(
+            `${
+              process.env.NEXT_PUBLIC_WORDPRESS_URL
+            }/redirection/v1/redirect?per_page=50&page=${
+              page + 1
+            }&filterBy[status]=enabled&filterBy[url]=/help/`,
+            fetchConfig
+          )
+        )
+      );
+      const allResponses = await Promise.all(
+        redirectPages.map((res) => res.json())
+      );
+      const allRedirects = allResponses.reduce(
+        (arr, res) => [...arr, ...res.items],
+        []
+      );
 
-      return json.items.map((r) => ({
+      return allRedirects.map((r) => ({
         source: `${r.url}/`,
         destination: `${r.action_data.url}/`,
         permanent: true,
