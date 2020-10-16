a1255119160b424e6658fdc30d08a3df4160ef3d
Ed Brett <e.j.a.brett@gmail.com>
2020-10-16 16:16:52 +0100

    update sitemap settings

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,5 +2,6 @@ module.exports = {
   siteUrl: 'https://www.globalforestwatch.org/help/',
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  priority: 1.0,
   exclude: ['/404', '/search/[query]'],
 };
