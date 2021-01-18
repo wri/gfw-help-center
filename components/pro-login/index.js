ab53da6a55acb2a7af989a2653dcd0ef752b8345
Tomas Eriksson <tomasmoosee@gmail.com>
2021-01-18 11:32:42 +0100

    cleanup pro login component

--- components/pro-login/index.js
+++ components/pro-login/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import ProLogo from 'assets/images/GFW_PRO-logo.png';
 
 import {
   Form,
   Submit,
-  Checkbox,
   Input,
-  validations,
-  Loader,
+  //   validations,
+  //   Loader,
 } from 'gfw-components';
 
 import {
@@ -18,73 +17,55 @@ import {
   LoginContainer,
   LoginTitle,
   RequestAccountTitle,
-  RequestAccountBtn
+  RequestAccountBtn,
 } from './styles';
 
-const handleSubmit = props => {
+const handleSubmit = (props) => {
   fetch('/help/api/pro', {
     method: 'POST',
-    body: JSON.stringify({
-      ...props,
-      persist: props.persist ? props.persist.length > 0 : false
-    })
-  }).then(r => r.json()).then(response => {
-    if (response.pro) {
-      window.location.reload();
-    }
-    // TODO: Error handling
+    body: JSON.stringify(props),
   })
-}
+    .then((r) => r.json())
+    .then((response) => {
+      if (response.pro) {
+        window.location.reload();
+      }
+      // TODO: Error handling
+    });
+};
 
-const ProLogin = ({ links }) => (
+const ProLogin = () => (
   <LoginWrapper>
     <LogoContainer>
       <img src={ProLogo} alt="GFW Pro" />
     </LogoContainer>
     <LoginContainer>
       <Form onSubmit={handleSubmit}>
-        {({ handleSubmit }) => (
+        {(
+          { handleSubmit } // eslint-disable-line
+        ) => (
           <form onSubmit={handleSubmit}>
             <LoginTitle>Log in to GFW Pro to continue</LoginTitle>
-            <Input
-              name="username"
-              label="Username"
-              required
-            />
-            <Input
-              name="password"
-              label="Password"
-              type="password"
-              required
-            />
-            <Checkbox
-              name="persist"
-              label=""
-              options={[
-                {
-                  label: 'Remember me',
-                  value: '0'
-                }
-              ]}
-            />
-            <Submit>
-              Login
-            </Submit>
+            <Input name="username" label="Username" required />
+            <Input name="password" label="Password" type="password" required />
+            <Submit>Login</Submit>
           </form>
-      )}
-    </Form>
-    <RequestAccountTitle><span>Don't have an account?</span></RequestAccountTitle>
-    <RequestAccountBtn
-      href="https://pro.globalforestwatch.org/account"
-      target="__BLANK"
-      rel="noreferrer"
-    >Request an account</RequestAccountBtn>
+        )}
+      </Form>
+      <RequestAccountTitle>
+        <span>Don&apos;t have an account?</span>
+      </RequestAccountTitle>
+      <RequestAccountBtn
+        href="https://pro.globalforestwatch.org/account"
+        target="__BLANK"
+        rel="noreferrer"
+      >
+        Request an account
+      </RequestAccountBtn>
     </LoginContainer>
   </LoginWrapper>
 );
 
-ProLogin.propTypes = {
-  // links: PropTypes.array,
-};
+ProLogin.propTypes = {};
 
 export default ProLogin;
