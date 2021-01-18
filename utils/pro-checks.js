c1039e4a288861e3b4c82ba26bd7503966e0e090
Tomas Eriksson <tomasmoosee@gmail.com>
2021-01-18 11:23:43 +0100

    setup relative path for pro check

--- utils/pro-checks.js
+++ utils/pro-checks.js
@@ -1,6 +1,6 @@
+/* eslint import/prefer-default-export: 0 */
 export const isProAuthenticated = async () => {
-  const response = await fetch('http://localhost:3000/help/api/pro');
+  const response = await fetch('/help/api/pro');
   const res = await response.json();
-
   return res;
-}
+};
