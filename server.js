a341cde157d7f69431c2a27095f3b98ca42da821
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-12-07 14:40:42 +0000

    Redirect from non-www to www in production

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+/**
+ * Redirects non-www urls to www urls
+ */
+function handleNonWwwToWwwRedirect(req, res) {
+  try {
+    const host = req.header('host');
+    if (!host.match(/^www\..*/i)) {
+      res.redirect(301, `https://www.${host}${req.url}`);
+    }
+  } catch (_i) {
+    // Ignore by default
+  }
+}
+
 app.prepare().then(() => {
   const server = express();
 
@@ -14,6 +28,13 @@ app.prepare().then(() => {
   server.use(sslRedirect(['production'], 301));
 
   server.all(/.*/, (req, res) => {
+    // Redirect from non-www to www, but only on actual `production`.
+    // Note that we cannot use `NODE_ENV` for this. Instead we need to use
+    //  the `NEXT_PUBLIC_FEATURE_ENV` environment variable.
+    if (process.env.NEXT_PUBLIC_FEATURE_ENV === 'production') {
+      handleNonWwwToWwwRedirect(req, res);
+    }
+
     return handle(req, res);
   });
 
