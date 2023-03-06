429c80deb1327186ba5205fdda36ccce11545be5
Tiago Garcia <tiagojsag@gmail.com>
2023-03-06 15:34:11 +0100

    Pick body values submited to the api when requesting a webinar

--- components/forms/request-webinar/actions.js
+++ components/forms/request-webinar/actions.js
@@ -1,8 +1,12 @@
 import { FORM_ERROR } from 'gfw-components';
 import { post } from 'axios';
+import { pick } from 'lodash';
 
 const requestWebinar = (body) => {
-  post('https://api.resourcewatch.org/form/request-webinar', body)
+  post(
+    'https://api.resourcewatch.org/form/request-webinar',
+    pick(body, ['name', 'email', 'request'])
+  )
     .then(() => {})
     .catch((error) => {
       const { errors } = error.response && error.response.data;
