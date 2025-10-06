1b13bc2d37119c5edf911eb64ea448b2ac141972
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2025-10-05 21:56:40 -0600

    feat(FLAG-1370): add new menu style in help center

--- layouts/home/index.js
+++ layouts/home/index.js
@@ -29,65 +29,70 @@ const HomePage = ({ homepage, tools }) => {
 
   return (
     <Wrapper>
-      <Row>
-        <Column width={[1, 5 / 6, 2 / 3]}>
+      <Row
+        css={css`
+          margin-bottom: 30px;
+          max-width: 100%;
+          padding: 0 205px 0 65px;
+        `}
+      >
+        <Column width={[1, 1 / 4]}>
+          <Row>
+            <SearchWrapper>
+              <Search expanded />
+            </SearchWrapper>
+          </Row>
+          <Row>hola</Row>
+        </Column>
+        <Column width={[1, 3 / 4]}>
           <Intro
             title={homepage?.title}
             description={ReactHtmlParser(homepage?.excerpt)}
           />
-        </Column>
-        <SearchWrapper>
-          <Search expanded />
-        </SearchWrapper>
-      </Row>
-      <Row
-        css={css`
-          margin-bottom: 30px;
-        `}
-      >
-        <Column>
           <ToolsTitle>Getting started on the GFW tools</ToolsTitle>
-        </Column>
-        {primaryTools?.map((tool, i) => (
-          <ToolCardsWrapper width={[1, 1 / 2]} key={tool.id}>
-            {i === 0 && (
-              <Prompt>
-                <Tag>Most people start here!</Tag>
-                <Desktop>
-                  <Arrow>
-                    <ArrowIcon alt="highlighting first card" />
-                  </Arrow>
-                </Desktop>
-              </Prompt>
-            )}
-            <Link href={tool.link}>
-              <ToolCard
-                active={i === 0}
-                {...tool}
-                text={ReactHtmlParser(tool?.excerpt)}
-              />
-            </Link>
-          </ToolCardsWrapper>
-        ))}
-      </Row>
-      <Row>
-        <Column>
-          <ToolsTitle>Using global forest watch</ToolsTitle>
-        </Column>
-        {secondaryTools?.map((tool, index) => {
-          const isFirst = index === 0;
+          <Row>
+            {primaryTools?.map((tool, i) => (
+              <ToolCardsWrapper width={[1, 1 / 2]} key={tool.id}>
+                {i === 0 && (
+                  <Prompt>
+                    <Tag>Most people start here!</Tag>
+                    <Desktop>
+                      <Arrow>
+                        <ArrowIcon alt="highlighting first card" />
+                      </Arrow>
+                    </Desktop>
+                  </Prompt>
+                )}
+                <Link href={tool.link}>
+                  <ToolCard
+                    active={i === 0}
+                    {...tool}
+                    text={ReactHtmlParser(tool?.excerpt)}
+                  />
+                </Link>
+              </ToolCardsWrapper>
+            ))}
+          </Row>
+          <Row>
+            <Column>
+              <ToolsTitle>Using global forest watch</ToolsTitle>
+            </Column>
+            {secondaryTools?.map((tool, index) => {
+              const isFirst = index === 0;
 
-          return (
-            <ToolCardsWrapper
-              {...(!isFirst && {
-                width: [1, 1 / 4],
-              })}
-              key={tool.id}
-            >
-              <SimpleCard {...tool} large={isFirst} />
-            </ToolCardsWrapper>
-          );
-        })}
+              return (
+                <ToolCardsWrapper
+                  {...(!isFirst && {
+                    width: [1, 1 / 4],
+                  })}
+                  key={tool.id}
+                >
+                  <SimpleCard {...tool} large={isFirst} />
+                </ToolCardsWrapper>
+              );
+            })}
+          </Row>
+        </Column>
       </Row>
     </Wrapper>
   );
